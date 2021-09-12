faec281945d7a251b3327d7638a5b0f3d0fc312e
Lucas H. Fernandes <luc.luc.fernandes852@gmail.com>
2021-09-12 15:48:52 -0300

    .

--- lib/ui/mainLucas.dart
+++ lib/ui/mainLucas.dart
@@ -1,9 +1,41 @@
 // @dart=2.9
-import 'package:app_teste/components/rounded_buttonLucas.dart';
 import 'package:flutter/material.dart';
-import 'package:app_teste/welcome_screenLucas.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
+
+void main() => runApp(Myapp());
+
+class Myapp extends StatelessWidget {
+  //raiz da aplicação.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Batalha naval',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+//import 'package:app_teste/Screens/components/bodyLucas.dart';
+//import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
+
+//import 'package:app_teste/components/rounded_buttonLucas.dart';
+//import 'package:flutter/material.dart';
+//import 'package:app_teste/welcome_screenLucas.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
+//import 'dart:convert';
 
 class Body extends StatefulWidget {
   @override
@@ -81,3 +113,55 @@ class _Homepagestate extends State<Body> {
     );
   }
 }
+
+//import 'package:app_teste/Screens/constantsLucas.dart';
+//import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Function press;
+  final Color color, textColor;
+  const RoundedButton({
+    Key key,
+    this.text,
+    this.press,
+    this.color = kPrimaryColor,
+    this.textColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      width: size.width * 0.5,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: newElevatedButton(),
+      ),
+    );
+  }
+
+  //Used:ElevatedButton as FlatButton is deprecated.
+  //Here we have to apply customizations to Button by inheriting the styleFrom
+
+  Widget newElevatedButton() {
+    return ElevatedButton(
+      child: Text(
+        text,
+        style: TextStyle(color: textColor),
+      ),
+      onPressed: press,
+      style: ElevatedButton.styleFrom(
+          primary: color,
+          padding: EdgeInsets.symmetric(horizontal: 35, vertical: 30),
+          textStyle: TextStyle(
+              color: textColor, fontSize: 14, fontWeight: FontWeight.w500)),
+    );
+  }
+}
+
+//import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
