8f317933cc5ae785923c63529faf830fb81bca90
GGsouza-18 <gabriel_swl@outlook.com>
2021-11-28 19:48:13 -0300

    Correcao Rotas

--- lib/ui/api_sign_up.dart
+++ lib/ui/api_sign_up.dart
@@ -2,10 +2,12 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:proj0511/rotas.dart';
+
 class ApiSignUp {
   static Future<bool> signUp(
       String name, String user, String email, String password) async {
-    var url = 'http://3.144.90.4:3333/usuarios';
+    var url = url1 + '/usuarios';
 
     var header = {"Content-Type": "application/json"};
 
