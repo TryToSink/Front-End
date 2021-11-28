51abd4499a963e365b98f7d7eb2d2392044aed0f
lucashfer <90424137+lucashfer@users.noreply.github.com>
2021-11-28 14:02:42 -0300

    2.3v

---
+++ lib/DTO/barcosDTO.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class barcosDTO {
+  String IDBarco = '';
+  String foto1 = '';
+  String foto2 = '';
+  String foto3 = '';
+  String foto4 = '';
+  String foto5 = '';
+  String nomeBarco = '';
+  int tamanho = 0;
+
+}
