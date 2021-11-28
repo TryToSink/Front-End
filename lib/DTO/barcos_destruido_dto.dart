4b34b9fd0e74a1297b85443f37b56701e4aa8861
Christian Dubois <chris280400@gmail.com>
2021-11-28 15:30:59 -0300

    123

---
+++ lib/DTO/barcos_destruido_dto.dart
@@ -0,0 +1,8 @@
+import 'package:proj0511/DTO/barcosDTO.dart';
+import 'package:proj0511/DTO/posicoesDTO.dart';
+
+class barcosDestruidoDTO {
+  bool destruido = false;
+  barcosDTO barco = new barcosDTO();
+  List<posicoesDTO> posicoes = [];
+}
--- lib/DTO/partidaAleatoriaDTO.dart
+++ lib/DTO/partidaAleatoriaDTO.dart
@@ -1,10 +1,4 @@
-import 'dart:convert';
-import 'dart:html';
-
-import 'package:flutter/material.dart';
 import 'package:proj0511/DTO/barcosDTO.dart';
-import 'package:socket_io_client/socket_io_client.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class partidaAleatoriaDTO {
   String? elo;
